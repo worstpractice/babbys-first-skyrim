471aa35fdf836a20554e8d2a9fd2d517edbd1f73
worstpractice <54796463+worstpractice@users.noreply.github.com>
2021-08-29 00:33:03 +0200

    fix

--- src/utils/snitch.ts
+++ src/utils/snitch.ts
@@ -3,5 +3,5 @@ import type { SetEvent } from 'obset';
 export const snitch = <T>({ operation, value }: SetEvent<T>) => {
   const emoji = operation === 'add' ? '➕' : '➖';
 
-  console.log(`${emoji} ${value}`);
+  console.log(`${emoji} ${operation} ${value}`);
 };
