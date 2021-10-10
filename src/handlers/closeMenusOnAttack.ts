a9b293de847170c3c42ade3cae6d02676b8947bc
worstpractice <54796463+worstpractice@users.noreply.github.com>
2021-10-10 22:52:30 +0200

    void

--- src/handlers/closeMenusOnAttack.ts
+++ src/handlers/closeMenusOnAttack.ts
@@ -1,7 +1,7 @@
 import { CANVAS } from 'src/constants/CANVAS';
 import { useUiState } from 'src/state/UiState';
 
-export const closeMenusOnAttack = ({ target }: MouseEvent) => {
+export const closeMenusOnAttack = ({ target }: MouseEvent): void => {
   if (target !== CANVAS) return;
 
   useUiState.setState({ currentOpenMenu: '' });
